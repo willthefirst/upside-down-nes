15860e690fa27150e48bce22172ef48125522008
Will Moritz <willthefirst@gmail.com>
2023-03-17 16:52:38 -0700

    Nice styles, almost ordered

--- pages/Article.tsx
+++ pages/Article.tsx
@@ -4,38 +4,49 @@ import { Figure } from "./Figure"
 export const Article = () => {
   return (
     <>
-      <h1>Banking Crisis Hangs Over Economy, Rekindling Recession Fear
-        Borrowing could become tougher, a particular blow to small businesses — and a threat to the recovery’s staying power.
-      </h1>
-      <h2>
-        Borrowing could become tougher, a particular blow to small businesses — and a threat to the recovery’s staying power.
-      </h2>
+      <style>{`
+        body {
+          padding: 0 1rem;
 
-      <p>The U.S. economic recovery has repeatedly defied predictions of an impending recession, withstanding supply-chain backlogs, labor shortages, global conflicts and the fastest increase in interest rates in decades.</p>
-      <p>That resilience now faces a new test: a banking crisis that, at times over the past week, seemed poised to turn into a full - blown financial meltdown as oil prices plunged and investors poured money into U.S.government debt and other assets perceived as safe.</p>
-      <p>Markets remained volatile on Friday — stocks had their worst day of the week — as leaders in Washington and on Wall Street sought to keep the crisis contained.</p>
-      <p>Even if those efforts succeed — and veterans of previous crises cautioned that was a big “if” — economists said the episode would inevitably take a toll on hiring and investments as banks pulled back on lending, and businesses struggled to borrow money as a result.Some forecasters said the turmoil had already made a recession more likely.</p>
-      <p>“There will be real and lasting economic repercussions from this, even if all the dust settles well,” said Jay Bryson, chief economist at Wells Fargo. “I would raise the probability of a recession given what’s happened in the last week.”</p >
-      <p>At a minimum, the crisis has complicated the already delicate task facing officials at the Federal Reserve, who have been trying to slow the economy gradually in order to bring inflation to heel. That task is as urgent as ever: Government data on Tuesday showed that prices continued to rise at a rapid clip in February. But now policymakers must grapple with the risk that the Fed’s efforts to fight inflation could be destabilizing the financial system.</p>
-      <p>They don’t have long to weigh their options: Fed officials will hold their next regularly scheduled meeting on Tuesday and Wednesday amid unusual uncertainty about what they will do.As recently as 10 days ago, investors expected the central bank to reaccelerate its campaign of interest rate increases in response to stronger - than - expected economic data.Now, Fed watchers are debating whether the meeting will end with rates unchanged.</p>
+          line-height: 1.4em;
+          font-size: 1.4rem;
+          font-family: Georgia, "Times New Roman", Times, serif;
+          color: #363636
+        }
+
+        img {
+          margin: auto;
+          width: 100%;
+          height: auto;
+        }
+
+        figcaption {
+          color: #727272;
+          font-size: 1.2rem;
+          line-height: 1.4em;
+        }
+
+        h1 {
+          font-style: italic;
+          font-weight: bold;
+          font-size: 2rem;
+          line-height: 1.4em
+        }
+
+        h2 {  
+          font-size: 1.2em;
+          line-height: 1.4em;
+          font-weight: 600
+        }
+
+        `}</style>
       <Figure
-        src="/17econ-moment-03-htcl-superJumbo.webp"
-        caption="The failure of Silicon Valley Bank, the midsize California institution, set the latest turmoil in motion. Ian C. Bates for The New York Times"
+        src="/17econ-moment-04-htcl-superJumbo.webp"
+        caption="Tougher lending standards could be a blow to small businesses and affect overall supply in the economy. Casey Steffens for The New York Times
+        "
       ></Figure>
       <p>
-        The notion that the rapid increase in interest rates could threaten financial stability is hardly new. In recent months, economists have remarked often that it is surprising that the Fed has been able to raise rates so much, so fast without severe disruptions to a marketplace that has grown used to rock-bottom borrowing costs.
-      </p>
-      <p>
-        What was less expected is where the first crack showed: small and midsize U.S. banks, in theory among the most closely monitored and tightly regulated pieces of the global financial system.
-      </p>
-      <p>
-        “I was surprised where the problem came, but I wasn’t surprised there was a problem,” Kenneth Rogoff, a Harvard professor and leading scholar of financial crises, said in an interview. In an essay in early January, he warned of the risk of a “looming financial contagion” as governments and businesses struggled to adjust to an era of higher interest rates.
-      </p>
-      <p>
-        He said he did not expect a repeat of 2008, when the collapse of the U.S. mortgage market quickly engulfed virtually the entire global financial system. Banks around the world are better capitalized and better regulated than they were back then, and the economy itself is stronger.
-      </p>
-      <p>
-        “Usually to have a more systemic financial crisis, you need more than one shoe to drop,” Professor Rogoff said. “Think of higher real interest rates as one shoe, but you need another.”
+        Already, there are hints that the crisis may not be limited to the United States. Credit Suisse said on Thursday that it would borrow up to $54 billion from the Swiss National Bank after investors dumped its stock as fears arose about its financial health. The 166-year-old lender has faced a long series of scandals and missteps, and its problems aren’t directly related to those of Silicon Valley Bank and other U.S. institutions. But economists said the violent market reaction was a sign that investors were growing concerned about the stability of the broader system.
       </p>
       <p>
         “If we’re not on top of that, then what about some of these other, more shadowy parts of the financial system?” said Anil Kashyap, a University of Chicago economist who studies financial crises.
@@ -44,13 +55,82 @@ export const Article = () => {
         Still, he and other experts said it was alarming that such severe problems could go undetected so long at Silicon Valley Bank, the midsize California institution whose failure set in motion the latest turmoil. That raises questions about what other threats could be lurking, perhaps in less regulated corners of finance such as real estate or private equity.
       </p>
       <p>
-        Already, there are hints that the crisis may not be limited to the United States. Credit Suisse said on Thursday that it would borrow up to $54 billion from the Swiss National Bank after investors dumped its stock as fears arose about its financial health. The 166-year-old lender has faced a long series of scandals and missteps, and its problems aren’t directly related to those of Silicon Valley Bank and other U.S. institutions. But economists said the violent market reaction was a sign that investors were growing concerned about the stability of the broader system.
+        “Usually to have a more systemic financial crisis, you need more than one shoe to drop,” Professor Rogoff said. “Think of higher real interest rates as one shoe, but you need another.”
+      </p>
+      <p>
+        He said he did not expect a repeat of 2008, when the collapse of the U.S. mortgage market quickly engulfed virtually the entire global financial system. Banks around the world are better capitalized and better regulated than they were back then, and the economy itself is stronger.
+
+
+      </p>
+      <p>
+        “I was surprised where the problem came, but I wasn’t surprised there was a problem,” Kenneth Rogoff, a Harvard professor and leading scholar of financial crises, said in an interview. In an essay in early January, he warned of the risk of a “looming financial contagion” as governments and businesses struggled to adjust to an era of higher interest rates.
+      </p>
+      <p>
+        What was less expected is where the first crack showed: small and midsize U.S. banks, in theory among the most closely monitored and tightly regulated pieces of the global financial system.
+      </p>
+      <p>
+        The notion that the rapid increase in interest rates could threaten financial stability is hardly new. In recent months, economists have remarked often that it is surprising that the Fed has been able to raise rates so much, so fast without severe disruptions to a marketplace that has grown used to rock-bottom borrowing costs.
+      </p>
+      <Figure
+        src="/17econ-moment-03-htcl-superJumbo.webp"
+        caption="The failure of Silicon Valley Bank, the midsize California institution, set the latest turmoil in motion. Ian C. Bates for The New York Times"
+      ></Figure>
+      <p>
+        “I was surprised where the problem came, but I wasn’t surprised there was a problem,” Kenneth Rogoff, a Harvard professor and leading scholar of financial crises, said in an interview. In an essay in early January, he warned of the risk of a “looming financial contagion” as governments and businesses struggled to adjust to an era of higher interest rates.
+      </p>
+      <p>
+        What was less expected is where the first crack showed: small and midsize U.S. banks, in theory among the most closely monitored and tightly regulated pieces of the global financial system.
       </p>
       <Figure
         src="/17econ-moment-04-htcl-superJumbo.webp"
-        caption="Tougher lending standards could be a blow to small businesses and affect overall supply in the economy. Casey Steffens for The New York Times
-        "
+        caption="Tougher lending standards could be a blow to small businesses and affect overall economic recovery. Erin Schaff for The New York Times"
       ></Figure>
+      <p>
+        Already, there are hints that the crisis may not be limited to the United States. Credit Suisse said on Thursday that it would borrow up to $54 billion from the Swiss National Bank after investors dumped its stock as fears arose about its financial health. The 166-year-old lender has faced a long series of scandals and missteps, and its problems aren’t directly related to those of Silicon Valley Bank and other U.S. institutions. But economists said the violent market reaction was a sign that investors were growing concerned about the stability of the broader system.
+      </p>
+      <p>
+        Still, he and other experts said it was alarming that such severe problems could go undetected so long at Silicon Valley Bank, the midsize California institution whose failure set in motion the latest turmoil. That raises questions about what other threats could be lurking, perhaps in less regulated corners of finance such as real estate or private equity.
+      </p>
+      <p>
+        “If we’re not on top of that, then what about some of these other, more shadowy parts of the financial system?” said Anil Kashyap, a University of Chicago economist who studies financial crises.
+      </p>
+
+      <p>
+        What was less expected is where the first crack showed: small and midsize U.S. banks, in theory among the most closely monitored and tightly regulated pieces of the global financial system.
+      </p>
+      <p>
+        The notion that the rapid increase in interest rates could threaten financial stability is hardly new. In recent months, economists have remarked often that it is surprising that the Fed has been able to raise rates so much, so fast without severe disruptions to a marketplace that has grown used to rock-bottom borrowing costs.
+      </p>
+      <Figure
+        src="/17econ-moment-03-htcl-superJumbo.webp"
+        caption="The failure of Silicon Valley Bank, the midsize California institution, set the latest turmoil in motion. Ian C. Bates for The New York Times"
+      ></Figure>
+
+      <p>
+        They don’t have long to weigh their options: Fed officials will hold their next regularly scheduled meeting on Tuesday and Wednesday amid unusual uncertainty about what they will do. As recently as 10 days ago, investors expected the central bank to reaccelerate its campaign of interest rate increases in response to stronger-than-expected economic data. Now, Fed watchers are debating whether the meeting will end with rates unchanged.
+      </p>
+
+
+      <p>
+        “There will be real and lasting economic repercussions from this, even if all the dust settles well,” said Jay Bryson, chief economist at Wells Fargo. “I would raise the probability of a recession given what’s happened in the last week.”
+      </p>
+
+      <p>Even if those efforts succeed — and veterans of previous crises cautioned that was a big “if” — economists said the episode would inevitably take a toll on hiring and investments as banks pulled back on lending, and businesses struggled to borrow money as a result. Some forecasters said the turmoil had already made a recession more likely.
+      </p>
+
+      <p>Markets remained volatile on Friday — stocks had their worst day of the week — as leaders in Washington and on Wall Street sought to keep the crisis contained.  </p>
+
+      <p>
+        The U.S. economic recovery has repeatedly defied predictions of an impending recession, withstanding supply-chain backlogs, labor shortages, global conflicts and the fastest increase in interest rates in decades.
+      </p>
+
+      <h2>
+        Borrowing could become tougher, a particular blow to small businesses — and a threat to the recovery’s staying power.
+      </h2>
+      <h1>Banking Crisis Hangs Over Economy, Rekindling Recession Fear
+        Borrowing could become tougher, a particular blow to small businesses — and a threat to the recovery’s staying power.
+      </h1>
+
     </>
   )
 }
